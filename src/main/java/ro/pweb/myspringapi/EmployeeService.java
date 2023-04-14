2b9cfecd9a3946d2135740857693f94de666c4a6
Marina Simionescu <marina.simionescu21@gmail.com>
2023-04-14 19:56:25 +0300

    first commit

---
+++ src/main/java/ro/pweb/myspringapi/EmployeeService.java
@@ -0,0 +1,19 @@
+package ro.pweb.myspringapi;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
